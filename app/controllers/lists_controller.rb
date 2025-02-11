e597eec96a4be8a2adebd2c57fb450b99b42564f
kuropAqn <kuropan0819@gmail.com>
2025-02-11 08:11:00 +0000

    0007

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,15 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id]) 
   end
 
   def edit
@@ -20,6 +22,6 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).parmit(:title,:body)
+    params.require(:list).permit(:title,:body)
   end
 end
